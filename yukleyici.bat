94974dd2feebad7d7954a4827aa8f94641b4232c
mehmetdopamine39-hub <mehmetdopamine39@gmail.com>
2026-07-26 09:06:15 +0300

    Delete yukleyici.bat

--- yukleyici.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Paketler Yukleniyor, Lutfen Bekleyiniz...
-npm install express-rate-limit axios crc-32 crypto jsdom dotenv mysql
-echo.
-echo Yukleme Tamamlandi!
-pause
